ddfb75055e052e591b6fe62a98099e462348c7cf
Ethan Torres <torresethan99@gmail.com>
2020-06-23 01:31:47 -0500

    codeQuiz: added more content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,44 @@
+
+//creates varables
 var start = document.getElementById("start");
-var submit = document.querySelector("button.submit")
+var question = document.getElementById("question");
+var submit = document.querySelector("button.submit");
+var time = document.getElementById("time");
+var remainingTime = (questions.length * 20 + 1);
+var userName;
+var ansSelect = document.getElementById("ans");
+var userScore = document.getElementById("userScore");
+var submitScore = document.querySelector("#submitScore");
+var questionNum = -1;
+var ans;
+
+
+
+
+// shows the current score
+
+function showScore() {
+
+}
+
+// calls the function to set the timer
+
+function startTimer() {
+    document.getElementById("homepage")
+}
+//------------end of start timer function-------------------------------------------------------
+
+
+
+// calls in a question
+
+function pullQuestion() {
+
+}
+//------------end of question pull function-----------------------------------------------------
+
+
+
+
+// when timer runs out
+
